65302d9dcc0b7b87f377e642628978d8d9f6508c
Matt Aboset <matt.aboset@gmail.com>
2017-05-19 18:16:50 -0400

    modified method names in the comment section

--- src/circular/doubly/linkedlist/CDoublyLinkedList.java
+++ src/circular/doubly/linkedlist/CDoublyLinkedList.java
@@ -15,11 +15,11 @@
  *       add() 					- adds new element to the list
  *       addFront(element) 		- adds new element to the front of the list
  *       addBack(element) 		- adds new element to the back of the list
- *       addPosition(element, position) - adds new element at the specified position
+ *       addAtPosition(element, position) - adds new element at the specified position
  *       remove(element) 		- removes the specified element
  *       removeFront() 			- removes the front element
  *       removeBack() 			- removes the last element
- *       removePosition(position) - removes the element in the specified position
+ *       removeAtPosition(position) - removes the element in the specified position
  *       toString() 			- outputs the elements in the list in forward order
  *       printReverse() 		- outputs the elements in the list in reverse order
  *       
